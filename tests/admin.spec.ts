ee54aa7e1e9616193219a70e9eca22a89c5aa378
wjchorak <westonjchorak@gmail.com>
2026-02-06 14:42:13 -0700

    add(test) login mock for admin

--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -1,7 +1,27 @@
 import { Page } from '@playwright/test';
 import { test, expect } from 'playwright-test-coverage';
+import { User, Role } from '../src/service/pizzaService';
 
 async function adminInit(page: Page) {
+  let loggedInUser: User | undefined;
+  const validUsers: Record<string, User> = { 'd@jwt.com': { id: '3', name: 'Kai Chen', email: 'd@jwt.com', password: 'a', roles: [{ role: Role.Admin }] } };
+  
+  await page.route('*/**/api/auth', async (route) => {
+    const loginReq = route.request().postDataJSON();
+    const user = validUsers[loginReq.email];
+    if (!user || user.password !== loginReq.password) {
+      await route.fulfill({ status: 401, json: { error: 'Unauthorized' } });
+      return;
+    }
+    loggedInUser = validUsers[loginReq.email];
+    const loginRes = {
+      user: loggedInUser,
+      token: 'abcdef',
+    };
+    expect(route.request().method()).toBe('PUT');
+    await route.fulfill({ json: loginRes });
+  });
+  
   await page.goto('/');
 }
 
