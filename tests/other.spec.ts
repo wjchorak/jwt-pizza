b21d7538dc04a6bf8147d8e78bccdc734e74f9a5
wjchorak <westonjchorak@gmail.com>
2026-02-07 15:12:42 -0700

    add(test) history, about, home pages recorded

---
+++ tests/other.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('/');
+
+  //homepage
+  await expect(page.getByText('The web\'s best pizza', { exact: true })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Order now' })).toBeVisible();
+  await expect(page.getByRole('main')).toContainText('Pizza is an absolute delight that brings joy to people of all ages. The perfect combination of crispy crust, savory sauce, and gooey cheese makes pizza an irresistible treat. At JWT Pizza, we take pride in serving the web\'s best pizza, crafted with love and passion. Our skilled chefs use only the finest ingredients to create mouthwatering pizzas that will leave you craving for more. Whether you prefer classic flavors or adventurous toppings, our diverse menu has something for everyone. So why wait? Indulge in the pizza experience of a lifetime and visit JWT Pizza today!');
+  await expect(page.getByRole('main').getByRole('img')).toBeVisible();
+  await expect(page.getByRole('contentinfo')).toContainText('© 2024 JWT Pizza LTD. All rights reserved. Version: 20000101.000000');
+  await expect(page.getByText('© 2024 JWT Pizza LTD. All')).toBeVisible();
+
+  //about
+  await page.getByRole('link', { name: 'About' }).click();
+  await expect(page.getByRole('main')).toContainText('The secret sauce');
+  await expect(page.getByRole('img').nth(3)).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Our employees' })).toBeVisible();
+
+  //history
+  await page.getByRole('link', { name: 'History' }).click();
+  await expect(page.getByRole('heading')).toContainText('Mama Rucci, my my');
+  await expect(page.getByRole('main').getByRole('img')).toBeVisible();
+});
