05d1ef615aa7635f34c499f6f0937abeab085854
wjchorak <westonjchorak@gmail.com>
2026-02-10 16:33:14 -0700

    add(instruction) copied updateuser from instruction

--- src/views/dinerDashboard.tsx
+++ src/views/dinerDashboard.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import View from './view';
+import { CloseIcon } from '../icons';
+import { HSOverlay } from 'preline';
+import Button from '../components/button';
 import { pizzaService } from '../service/service';
 import { Order, OrderHistory, Role, User } from '../service/pizzaService';
 
@@ -21,6 +24,12 @@ export default function DinerDashboard(props: Props) {
     })();
   }, [user]);
 
+  async function updateUser() {
+    setTimeout(() => {
+      HSOverlay.close(document.getElementById('hs-jwt-modal')!);
+    }, 100);
+  }
+
   function formatRole(role: { role: Role; objectId?: string }) {
     if (role.role === Role.Franchisee) {
       return `Franchisee on ${role.objectId}`;
@@ -34,6 +43,7 @@ export default function DinerDashboard(props: Props) {
       <div className="text-start py-8 px-4 sm:px-6 lg:px-8">
         <div className="hs-tooltip inline-block">
           <img className="hs-tooltip-toggle relative inline-block size-[96px] rounded-full ring-2 ring-white hover:z-10" src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80" alt="Employee stock photo" />
+          <Button title="Edit" className="w-16 p-0" onPress={() => HSOverlay.open(document.getElementById('hs-jwt-modal')!)} />
         </div>
 
         <div className="my-4 text-lg text-orange-200 text-start grid grid-cols-5 gap-2">
@@ -98,6 +108,27 @@ export default function DinerDashboard(props: Props) {
             </div>
           </>
         )}
+      </div>
+
+      <div role="dialog" aria-modal="true" aria-labelledby="dialog-title" id="hs-jwt-modal" className="hs-overlay hidden size-full fixed top-10 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none">
+        <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-3.5rem)]">
+          <div className="w-full flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto   ">
+            <div className="flex justify-between items-center py-3 px-4 border-b bg-slate-200 rounded-t-xl ">
+              <h3 className="font-bold text-gray-800">Edit user</h3>
+              <button type="button" className="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none" data-hs-overlay="#hs-jwt-modal">
+                <CloseIcon className="" />
+              </button>
+            </div>
+            <div className="p-4 overflow-y-scroll max-h-52">
+              <div className="my-4 text-lg text-start grid grid-cols-5 gap-2 items-center">update fields here</div>
+            </div>
+            <div className="flex justify-end items-center gap-x-2 py-3 px-4 border-t  bg-slate-200 rounded-b-xl">
+              <button type="button" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" onClick={updateUser}>
+                Update
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </View>
   );
