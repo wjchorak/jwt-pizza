a37a0d53f243be1b2d2e4b3b9c78b3e4f6d2d3d9
wjchorak <westonjchorak@gmail.com>
2026-02-16 17:01:39 -0700

    enhance(test) test for email and password changes

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from 'playwright-test-coverage';
 
 test('updateUser', async ({ page }) => {
   const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+  const updatedEmail = `updated${Math.floor(Math.random() * 10000)}@jwt.com`;
+  const updatedPassword = 'newpassword';
 
   let currentUser = {
     id: 1,
@@ -79,4 +81,18 @@ test('updateUser', async ({ page }) => {
   await page.waitForSelector('[role="dialog"].hidden', { state: 'attached' });
 
   await expect(page.getByRole('main')).toContainText('pizza dinerx');
+
+  await page.getByRole('button', { name: 'Edit' }).click();
+  await expect(page.locator('h3')).toContainText('Edit user');
+
+  await page.getByRole('textbox').nth(0).fill('pizza diner updated');
+  await page.getByRole('textbox').nth(1).fill(updatedEmail);
+  await page.getByRole('textbox').nth(2).fill(updatedPassword);
+
+  await page.getByRole('button', { name: 'Update' }).click();
+
+  await page.waitForSelector('[role="dialog"].hidden', { state: 'attached' });
+
+  await expect(page.getByRole('main')).toContainText('pizza diner updated');
+  await expect(page.getByRole('main')).toContainText(updatedEmail);
 });
